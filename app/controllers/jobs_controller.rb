a13fcd352166eb590479b13e68d81b1f8ee03739
tinklexjane <tinklexjane@gmail.com>
2017-01-19 22:34:24 +0800

    add order

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,6 +3,14 @@ class JobsController < ApplicationController
 
   def index
     @jobs = Job.where(:is_hidden => false).order("created_at DESC")
+    @jobs = case params[:order]
+    when 'by_lower_bound'
+      Job.published.order('wage_lower_bound DESC')
+    when 'by_upper_bound'
+      Job.published.order('wage_upper_bound DESC')
+    else
+      Job.published.recent
+    end
   end
 
   def show
@@ -16,7 +24,7 @@ class JobsController < ApplicationController
   def create
     @job = Job.new(job_params)
     @job.user= current_user
-    if @job.save!
+    if @job.save
       redirect_to jobs_path, notice: 'Add Success'
     else
       render :new
